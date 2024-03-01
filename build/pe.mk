41164170bd830fe95219d5cb79c4dae2d16e28cb
Tiffany Fung <46277006+changanmoon@users.noreply.github.com>
2024-03-01 10:51:37 +0800

    Update and rename aosp.mk to pe.mk

---
+++ build/pe.mk
@@ -0,0 +1,4 @@
+$(call inherit-product, vendor/ponces/config/common.mk)
+
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    ro.system.ota.json_url=https://raw.githubusercontent.com/changanmoon/treble_pe/fourteen/config/ota.json
